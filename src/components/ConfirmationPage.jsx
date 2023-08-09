b041df876fef69f18610c1640b26c1118c7780d8
Deng Benjamin <Deng.benjamin.6@gmail.com>
2023-08-09 16:12:17 -0600

    - confirmation page

---
+++ src/components/ConfirmationPage.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router";
+import OrderConfirmation from "./OrderConfirmation";
+import styles from "./styles/ConfirmationPage.module.css";
+
+function ConfirmationPage() {
+  const { id } = useParams();
+  const [order, setOrder] = useState(null);
+
+  const getOrder = async (orderId) => {
+    try {
+      const response = await fetch(
+        `http://localhost:3001/api/orders/order/${orderId}`
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setOrder(data);
+    } catch (error) {
+      setOrder(null);
+    }
+  };
+
+  useEffect(() => {
+    getOrder(id);
+  }, [id]);
+
+  return (
+    <div className={styles["confirmation-page"]}>
+      {order ? (
+        <OrderConfirmation
+          name={order.name}
+          address={order.address}
+          items={order.items}
+          id={order.id}
+        />
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
+
+export default ConfirmationPage;
---
+++ src/components/OrderConfirmation.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styles from "./styles/OrderConfirmation.module.css";
+
+function OrderConfirmation({ name, address, items, id }) {
+  return (
+    <div className={styles["order-confirmation"]}>
+      <div className={styles["order-header"]}>
+        <div className={styles["order-subheader"]}>
+          <h2>Order #{id}</h2>
+          <p>Thank you for your order {name}</p>
+        </div>
+      </div>
+      <div className={styles["order-customer"]}>
+        <h2>Customer Information</h2>
+        <p>
+          <strong>Name:</strong> {name}
+        </p>
+        <p>
+          <strong>Address:</strong> {address}
+        </p>
+      </div>
+      <div className={styles["order-items"]}>
+        <h2>Your Order</h2>
+        <ul className={styles["item-list"]}>
+          {items.map((item) => (
+            <li key={`${item.id}-${Math.random()}`} className={styles.item}>
+              <p>
+                <strong>Name:</strong> {item.item.name}
+              </p>
+              <p>
+                <strong>Quantity:</strong> {item.quantity}
+              </p>
+              <p>
+                <strong>Price:</strong> ${item.item.price}
+              </p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default OrderConfirmation;
