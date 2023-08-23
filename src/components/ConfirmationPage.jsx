d8a035adedb0a563dbc9aeae55d6a7d5a14ebc9e
Clement Shum <clement.shum128@gmail.com>
2023-08-23 19:38:45 -0400

    confirmation page

--- src/components/ConfirmationPage.jsx
+++ src/components/ConfirmationPage.jsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom";
+import OrderConfirmation from "./OrderConfirmation"
 
 function ConfirmationPage() {
+  const { id } = useParams(); // Get the 'id' parameter from the URL
+  const [ order, setOrder ] = useState({})
+  // const getOrder = () => (
+  //   fetch(`/api/orders/${id}`)
+  //     .then((res) => res.json())
+  //     .then((data) => setOrder(data))
+  // )
+
+  const getOrder = async() => {
+    try{
+      const response = await fetch(`/api/orders/${id}`);
+      const data = await response.json();
+      setOrder(data);
+    }
+    catch (error) {
+      console.log('Error: ', error)
+    }
+  }
+
+  useEffect(() => {
+    getOrder()
+  }, [])
+
   return (
-    <div>
-        <h2>Thank you for your order!</h2>
-    </div>
+    <OrderConfirmation order={order}/>
   )
 }
 
