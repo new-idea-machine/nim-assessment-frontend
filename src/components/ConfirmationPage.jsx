4ab3b2e07a5b8fff65f3b5071e435bd8103e8a50
KovaKreative <eric.kovalevskyy@gmail.com>
2023-09-14 10:51:28 -0400

    Connect ConfirmationPage component to server and retrieve order data

--- src/components/ConfirmationPage.jsx
+++ src/components/ConfirmationPage.jsx
@@ -1,7 +1,18 @@
+import { useEffect } from "react";
 import { useParams } from "react-router";
 
 function ConfirmationPage() {
   const { id } = useParams();
+
+  const getOrder = async () => {
+    const response = await fetch(`/api/orders/${id}`);
+    const data = await response.json();
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getOrder();
+  }, []);
 
   return <div className="confirmation-page">CONFIRMATION PAGE {id}</div>;
 }
