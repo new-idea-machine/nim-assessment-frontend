dfa997214404a1a9c17de50ada827774cf75d852
KovaKreative <eric.kovalevskyy@gmail.com>
2023-09-14 09:14:13 -0400

    Add placeholder to empty component

--- src/components/ConfirmationPage.jsx
+++ src/components/ConfirmationPage.jsx
@@ -1,10 +1,14 @@
+import { useParams } from "react-router";
+
 function ConfirmationPage() {
+
+  const { id } = useParams();
 
   return (
     <div className="confirmation-page">
-      
+      CONFIRMATION PAGE {id}
     </div>
-  )
+  );
 }
 
 export default ConfirmationPage;
