34119d1b1e674dcdf0c4f6b7e4af47ea0c9a3bb7
Brendan Woo <woo.brendan@gmail.com>
2023-09-20 22:53:59 -0400

    import usenavigate and initialize as navigatee. create if block for response status 200, and navigate to order confirmation page with id passed from back end responsee

--- src/components/OrderModal.jsx
+++ src/components/OrderModal.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router";
 import styles from "./styles/OrderModal.module.css";
 
 function OrderModal({ order, setOrderModal }) {
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
   const [address, setAddress] = useState("");
+
+  const navigate = useNavigate()
 
   const placeOrder = async () => {
     const response = await fetch("/api/orders", {
@@ -19,8 +22,12 @@ function OrderModal({ order, setOrderModal }) {
         items: order
       })
     });
-    const data = await response.json();
-    console.log(data);
+
+    if (response.status === 200) {
+      const data = await response.json()
+      navigate(`/order-confirmation/${data.id}`)
+    }
+
   };
   return (
     <>
