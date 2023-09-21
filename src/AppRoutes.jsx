105e40cc5b24d7bbf9a92c997d7cddafc060865b
Brendan Woo <woo.brendan@gmail.com>
2023-09-20 21:19:08 -0400

    add route for order confirmation using confirmationpage componenet

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import ConfirmationPage from "./components/ConfirmationPage";
 import Main from "./components/Main";
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
+      <Route path="/order-confirmation/:id" element={<ConfirmationPage />} />
     </Routes>
   );
 }
