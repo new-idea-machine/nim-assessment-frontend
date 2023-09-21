49adad8436f123ce483bf1a246d1d87b30ff298a
Brendan Woo <woo.brendan@gmail.com>
2023-09-20 21:58:06 -0400

    crreate use state for orders, change named varriable orders inside getorders to be data. create useeffect to initialize getOrders. make return val of component be orderConfirmation component passing orders props

--- src/components/ConfirmationPage.jsx
+++ src/components/ConfirmationPage.jsx
@@ -1,9 +1,21 @@
+import React, { useState, useEffect } from "react"
+import OrderConfirmation from "./OrderConfirmation"
+
 function ConfirmationPage({ id }) {
+    const [orders, setOrders] = useState([])
     const getOrders = async ()  => {
         const response = await fetch(`/api/orders/${id}`)
-        const orders = await response.json()
+        const data = await response.json()
+        setOrders(data)
     }
-    return (<></>)
+
+    useEffect(() => {
+        getOrders()
+    }, [])
+
+    return (
+        <OrderConfirmation orders={orders} />
+    )
 }
 
 export default ConfirmationPage 
