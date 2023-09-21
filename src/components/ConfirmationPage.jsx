08cd9dd3d12adfd2423d57cadf446c7841727d97
Brendan Woo <woo.brendan@gmail.com>
2023-09-20 21:25:24 -0400

    create getOrders function to fetch api orders passing id value

--- src/components/ConfirmationPage.jsx
+++ src/components/ConfirmationPage.jsx
@@ -1,5 +1,9 @@
-function ConfirmationPage() {
+function ConfirmationPage({ id }) {
+    const getOrders = async ()  => {
+        const response = await fetch(`/api/orders/${id}`)
+        const orders = await response.json()
+    }
     return (<></>)
 }
 
-export default ConfirmationPage
+export default ConfirmationPage 
